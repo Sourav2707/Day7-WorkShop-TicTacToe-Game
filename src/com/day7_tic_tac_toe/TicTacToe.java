2a8ebca5f8b54b95f3bf77c337d96c5f27154607
Sourav2707 <smartsourav33@gmail.com>
2022-12-18 15:52:55 +0530

    UC1 TicTacToe fresh board with char[10] where 0th index is ignored

--- src/com/day7_tic_tac_toe/TicTacToe.java
+++ src/com/day7_tic_tac_toe/TicTacToe.java
@@ -2,11 +2,18 @@ package com.day7_tic_tac_toe;
 
 
 public class TicTacToe {
+    char[] board = new char[10]; //size 10 char[] with empty spaces
     public void welcome() {
         System.out.println("Welcome to day 7 workshop program - TicTacToe game");
     }
     public void uc1_CreatingGame() {
-        char[] board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}; //size 10 char[] with empty spaces and ignoring 0th index
+        board[1] = ' '; //ignoring 0th index
+        board[2] = ' ';
+        board[3] = ' ';
+        board[4] = ' ';
+        board[5] = ' ';
+        board[6] = ' ';
+        board[7] = ' ';
         System.out.println("Fresh board: ");
         System.out.println(board[1]+" | "+board[2]+" | "+board[3]);
         System.out.println(board[4]+" | "+board[4]+" | "+board[6]);
