300defa9fba4bcad0576307d39b09b407f2a6767
Sourav2707 <smartsourav33@gmail.com>
2022-12-18 14:20:36 +0530

    UC2 player choosing whether his option is X or O

--- src/com/day7_tic_tac_toe/TicTacToe.java
+++ src/com/day7_tic_tac_toe/TicTacToe.java
@@ -1,4 +1,6 @@
 package com.day7_tic_tac_toe;
+import java.util.Scanner;
+
 public class TicTacToe {
     static final char a = 0; // inorder to ignore 0th index
 
@@ -12,5 +14,20 @@ public class TicTacToe {
         System.out.println(board[1]+" | "+board[2]+" | "+board[3]);
         System.out.println(board[4]+" | "+board[4]+" | "+board[6]);
         System.out.println(board[7]+" | "+board[9]+" | "+board[9]);
+    }
+    public static void uc2_chooseXO() {
+        System.out.println("Select letter X or O as your option: ");
+        Scanner sc = new Scanner(System.in);
+        char userInput = sc.next().charAt(0);
+        if(userInput == 'X' || userInput == 'x') {
+            System.out.println("Player option is X");
+            System.out.println("Computer option is O");
+        }
+        else if(userInput == 'O' || userInput == 'o'){
+            System.out.println("Player option is O");
+            System.out.println("Computer option is X");
+        }
+        else
+            System.out.println("Invalid input only X or O is allowed");
     }
 }
