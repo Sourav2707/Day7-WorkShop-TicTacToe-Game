3f9678d0255411cda94410f1f55ac16883169b7a
Sourav2707 <smartsourav33@gmail.com>
2022-12-20 18:00:53 +0530

    UC7 checking whether the game is win or tie

--- src/com/day7_tic_tac_toe/TicTacToe.java
+++ src/com/day7_tic_tac_toe/TicTacToe.java
@@ -10,9 +10,11 @@ public class TicTacToe {
     int playerToss;
     int tie;
     int win = 0;
+
     public void welcome() {
         System.out.println("Welcome to day 7 workshop program - TicTacToe game");
     }
+
     public void uc1_CreatingGame() {
         //ignoring 0th index
         board[1] = ' '; //ignoring 0th index
@@ -25,10 +27,11 @@ public class TicTacToe {
         board[8] = ' ';
         board[9] = ' ';
         System.out.println("Fresh board: ");
-        System.out.println(board[1]+" | "+board[2]+" | "+board[3]);
-        System.out.println(board[4]+" | "+board[4]+" | "+board[6]);
-        System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println(board[4] + " | " + board[4] + " | " + board[6]);
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
     }
+
     public void uc2_chooseXO() {
         System.out.println("Select letter X or O as your option: ");
         userInput = sc.next().charAt(0);
@@ -43,93 +46,66 @@ public class TicTacToe {
         } else
             System.out.println("Invalid input only X or O is allowed");
     }
+
     public void uc3_showBoard() {
-        System.out.println(board[1]+" | "+board[2]+" | "+board[3]);
-        System.out.println(board[4]+" | "+board[4]+" | "+board[6]);
-        System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println(board[4] + " | " + board[4] + " | " + board[6]);
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
     }
+
     public void uc4_makeAMove() {
         System.out.println("Enter the position number: ");
-        int move = sc.nextInt();board[move] = userInput;
+        int move = sc.nextInt();
+        board[move] = userInput;
         uc3_showBoard();
     }
+
     public void uc5_checkFreeSpace() {
-        for(int i = 1; i < board.length; i++) {
-            if(board[i] == ' ')
-                System.out.print(i+" ");
+        for (int i = 1; i < board.length; i++) {
+            if (board[i] == ' ')
+                System.out.print(i + " ");
         }
         System.out.println("Positions are free to occupy");
     }
+
     public void uc6_toss() {
         Random rnd = new Random();
-        int toss = rnd.nextInt(0,2);
+        int toss = rnd.nextInt(0, 2);
         System.out.println("Enter 1 for head and 0 for tail in order to check who plays first");
-         playerToss = sc.nextInt();
-         if(toss == playerToss && toss == 1) {
-             System.out.println("Heads player won the toss");
-             uc4_makeAMove();
-         }
-         else if(toss == playerToss && toss == 0) {
-             System.out.println("Tails, player won the toss");
-             uc4_makeAMove();
-         }
-    }
-    public void uc7_turn() {
-        String line1 = null;
-        String line2 = null;
-        for (int a = 0; a < 8; a++) {
-
-            switch (a) {
-                case 0:
-                    line1 = ""+board[0] + board[1] + board[2];
-                    line2 = ""+board[0] + board[1] + board[2];
-                    break;
-                case 1:
-                    line1 = ""+board[3] + board[4] + board[5];
-                    line2 = ""+board[3] + board[4] + board[5];
-                    break;
-                case 2:
-                    line1 = ""+board[6] + board[7] + board[8];
-                    line2 = ""+board[6] + board[7] + board[8];
-                    break;
-                case 3:
-                    line1 = ""+board[0] + board[3] + board[6];
-                    line2 = ""+board[0] + board[3] + board[6];
-                    break;
-                case 4:
-                    line1 = ""+board[1] + board[4] + board[7];
-                    line2 = ""+board[1] + board[4] + board[7];
-                    break;
-                case 5:
-                    line1 = ""+board[2] + board[5] + board[8];
-                    line2 = ""+board[2] + board[5] + board[8];
-                    break;
-                case 6:
-                    line1 = ""+board[0] + board[4] + board[8];
-                    line2 = ""+board[0] + board[4] + board[8];
-                    break;
-                case 7:
-                    line1 = ""+board[2] + board[4] + board[6];
-                    line2 = ""+board[2] + board[4] + board[6];
-                    break;
-                case 8:
-                    if(board[1] == ' ' || board[2] == ' ' || board[3] == ' ' ||
-                    board[4] == ' ' || board[5] == ' ' || board[6] == ' ' ||
-                    board[7] == ' ' || board[8] == ' ' || board[9] == ' ') {
-                        tie = 1;
-                    }
-                    break;
-
-            }
+        playerToss = sc.nextInt();
+        if (toss == playerToss && toss == 1) {
+            System.out.println("Heads player won the toss");
+            uc4_makeAMove();
+        } else if (toss == playerToss && toss == 0) {
+            System.out.println("Tails, player won the toss");
+            uc4_makeAMove();
         }
-        if(line1 == ""+userInput+userInput+userInput)
-            System.out.println("Player is the winner");
-        else if(line2 == ""+comInput+comInput+comInput)
-            System.out.println("Computer is the winner");
-        else if((line1 != ""+userInput+userInput+userInput) && (line2 != ""+comInput+comInput+comInput) && tie == 1 )
-            System.out.println("Game tie");
     }
-//    public void uc8_compTurn() {
-//        if(board[0] == ' ' || )
-//    }
+
+    public void uc7_turn() {
+        // the below condition is the winning combinations 123, 456, 789, 157, 357, 147, 258, 369
+        if ((board[1] == comInput && board[2] == comInput && board[3] == comInput) ||
+                (board[1] == comInput && board[4] == comInput && board[7] == comInput) ||
+                (board[1] == comInput && board[5] == comInput && board[9] == comInput) ||
+                (board[2] == comInput && board[5] == comInput && board[8] == comInput) ||
+                (board[3] == comInput && board[6] == comInput && board[9] == comInput) ||
+                (board[3] == comInput && board[5] == comInput && board[7] == comInput) ||
+                (board[4] == comInput && board[5] == comInput && board[6] == comInput) ||
+                (board[7] == comInput && board[8] == comInput && board[9] == comInput)) {
+            System.out.println("Computer win!");
+            win = 1;
+        } else if ((board[1] == userInput && board[2] == userInput && board[3] == userInput) ||
+                (board[1] == userInput && board[4] == userInput && board[7] == userInput) ||
+                (board[1] == userInput && board[5] == userInput && board[9] == userInput) ||
+                (board[2] == userInput && board[5] == userInput && board[8] == userInput) ||
+                (board[3] == userInput && board[6] == userInput && board[9] == userInput) ||
+                (board[3] == userInput && board[5] == userInput && board[7] == userInput) ||
+                (board[4] == userInput && board[5] == userInput && board[6] == userInput) ||
+                (board[7] == userInput && board[8] == userInput && board[9] == userInput)) {
+            System.out.println("Player win!");
+            win = 1;
+        } else if ((win == 1) || (board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' ' && board[5] != ' ' && board[6] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ')) {
+            System.out.println("Game tie");
+        }
+    }
 }
